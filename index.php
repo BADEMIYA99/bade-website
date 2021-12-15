62ae4d240a42d334f01cd6c68ca373094ac194e4
BADEMIYA99 <54698782+BADEMIYA99@users.noreply.github.com>
2021-12-15 13:27:34 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Bade-Website</title>
+	</head>
+
+	<body>
+		<h1>Hello All</h1>
+		<h2>Welcome to bade website</h2>
+		<?php
+	
+			echo " Learn Easily with Us "
+		
+		?>
+	</body>
+</html>
